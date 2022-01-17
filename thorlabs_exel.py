a01a976bd661f8a33852ed6532c6fba36132af81
Fei (Emily) Xia <fxialeo@gmail.com>
2022-01-17 13:03:06 +0100

    automatic change directory
    Auto set working directory. Avoid manual setting of the working directory, which sometimes causes import error if not set correctly at the beginning.

--- thorlabs_exel.py
+++ thorlabs_exel.py
@@ -7,12 +7,21 @@ Created on Fri Feb  5 21:47:56 2021
 
 """
 
+import os
 import numpy as np
 from datetime import date
-import google_trans_new_local
-from google_trans_new_local.google_trans_new import google_translator
 import openpyxl
 from openpyxl.styles.borders import Border, Side
+
+# INPUT
+source_dir = 'C:/Users/fxial/Desktop/thorlabsBC-main/thorlabsBC-main/' #directory where you save the original files from the github repo
+target_dir = 'C:/Users/fxial/Desktop/thorlabsBC-main/thorlabsBC-main/' #target directory where you want the output files to be saved
+file_path = target_dir + 'shoppingCart.xls' #
+discount = 0.09
+shipping_cost = 13.1 #need to be verified with actual quote
+os.chdir(source_dir)
+import google_trans_new_local
+from google_trans_new_local.google_trans_new import google_translator
 from nacres_from_thorlabs import nacres_from_thorlabs
 
 # define cell boarders join appropriate cells
@@ -24,12 +33,6 @@ def num2str(num):
         pos = len(output) - 6
         output = output[:pos] + '.' + output[pos:]
     return output
-
-# INPUT
-target_dir = '/Users/bernhard/Documents/Administration/LKB/Orders/20210715_Gianni_Thorlabs/' #need to be changed according to your own directory
-file_path = target_dir + 'shoppingCart.xls'
-discount = 0.09
-shipping_cost = 13.1 #need to be verified with actual quote
 
 # load input data - check first the existence of .xls (default from thorlabs site), 
 # then the .xlsx (from original implementation of this code)
